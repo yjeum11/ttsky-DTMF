f971d71d4edde57975ce58552738fd159c4af775
YJ Eum <yjeum11@gmail.com>
2026-07-18 16:58:05 +0900

    testing multiply

--- src/project.v
+++ src/project.v
@@ -17,11 +17,13 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uo_out  = ui_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = ui_in[3:0] * ui_in[7:4];
+  assign uio_oe = '1;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
+
+  
 
 endmodule
